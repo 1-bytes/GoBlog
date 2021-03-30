60a2c96466ebed910ff5db877851b8780b9a92fd
Enzo <1991996543@qq.com>
2021-03-31 06:18:42 +0800

    新建博文表单

--- main.go
+++ main.go
@@ -7,34 +7,43 @@ import (
 	"strings"
 )
 
+var router = mux.NewRouter()
+
+// homeHandler 首页.
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello，这里是goblog</h1>")
+	fmt.Fprint(w, "<h1>Hello，这里是blog</h1>")
 }
 
+// aboutHandler 关于.
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 		"<a href=\"mailto:abc@example.com\">abc@example.com</a>")
 }
 
+// notFoundHandler 404.
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
 
+// articlesShowHandler 文章详情 API接口.
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fmt.Fprint(w, "文章 ID："+id)
 }
 
+// articlesIndexHandler 文章列表 API接口.
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "访问文章列表")
 }
 
+// articlesStoreHandler 创建新的文章 API接口.
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
 }
 
+// forceHTMLMiddleware 中间件,用于设置返回的Header中的ContentType.
 func forceHTMLMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 设置Header
@@ -44,6 +53,7 @@ func forceHTMLMiddleware(h http.Handler) http.Handler {
 	})
 }
 
+// removeTrailingSlash 中间件,路由清理末尾的斜杠.
 func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
@@ -53,25 +63,43 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+// articlesCreateHandler 新建文章页面.
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>创建文章 —— 我的技术博客</title>
+	</head>
+	<body>
+		<form action="%s" method="post">
+			<p><input type="text" name="title"></p>
+			<p><textarea name="body" cols="30" rows="10"></textarea></p>
+			<p><button type="submit">提交</button></p>
+		</form>
+	</body>
+	</html>
+	`
+	storeURL, err := router.Get("articles.store").URL()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, html, storeURL)
+}
+
 func main() {
-	router := mux.NewRouter()
 	router.HandleFunc("/", homeHandler).Name("home")
 	router.HandleFunc("/about", aboutHandler).Name("about")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	// 自定义404页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
-	// 使用中间件
+	// 中间件：强制内容为 HTML
 	router.Use(forceHTMLMiddleware)
 
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articlesURL, _ := router.Get("articles.show").URL("id", "1")
-	fmt.Println("articlesURL: ", articlesURL)
-
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
-	return
 }
