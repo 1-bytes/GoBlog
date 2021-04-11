b8e98cded4bf54e94b7335b7cb9a619cc2d9a249
Enzo <1991996543@qq.com>
2021-04-11 23:01:46 +0800

    完成单元测试

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -3,16 +3,50 @@ package tests
 import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
-func TestHomePage(t *testing.T) {
+func TestAllPages(t *testing.T) {
 	baseUrl := "http://localhost:3000"
 
-	// 1. 请求 -> 模拟用户访问浏览器
-	resp, err := http.Get(baseUrl)
+	// 1. 声明加初始化测试数据
+	var tests = []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", http.StatusOK},
+		{"GET", "/about", http.StatusOK},
+		{"GET", "/notfound", http.StatusNotFound},
+		{"GET", "/articles", http.StatusOK},
+		{"GET", "/articles/create", http.StatusOK},
+		{"GET", "/articles/3", http.StatusOK},
+		{"GET", "/articles/3/edit", http.StatusOK},
+		{"POST", "/articles/3", http.StatusOK},
+		{"POST", "/articles", http.StatusOK},
+		{"POST", "/articles/1/delete", http.StatusNotFound},
+	}
 
-	// 2. 检测 -> 是否无错误且返回状态 200
-	assert.NoError(t, err, "有错误发生，err不为空")
-	assert.Equal(t, http.StatusOK, resp.StatusCode, "应返回状态码 200")
+	// 2. 遍历所有测试
+	for _, test := range tests {
+		t.Logf("当前请求 URL: %v \n", test.url)
+
+		var (
+			resp *http.Response
+			err  error
+		)
+
+		// 2.1 请求以获取响应
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseUrl+test.url, data)
+		default:
+			resp, err = http.Get(baseUrl + test.url)
+		}
+		// 2.2 断言
+		assert.NoError(t, err, "请求 "+test.url+" 时报错")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+" 应返回状态码 "+strconv.Itoa(resp.StatusCode))
+	}
 }
